59b1b9730c54df462ee1fc538f2ae80e6ce953e4
Luke Eades <lukeeadeswork@gmail.com>
2024-11-20 22:11:44 -0800

    completed functionality and styling

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,24 +14,27 @@ type Info struct {
 	Values []string
 }
 
-var distInfo Info = Info{"distance", []string{"inch", "foot", "mile", "meter", "kilometer"}}
+var distInfo Info = Info{"distance", []string{"inch", "foot", "mile", "millimeter", "centimeter", "meter", "kilometer"}}
 
 var distInMeters map[string]float64 = map[string]float64{
-	"inch":      .0254,
-	"foot":      .3048,
-	"mile":      1609.34,
-	"meter":     1,
-	"kilometer": 1000,
+	"inch":       .0254,
+	"foot":       .3048,
+	"mile":       1609.34,
+	"millimeter": .001,
+	"centimeter": .01,
+	"meter":      1,
+	"kilometer":  1000,
 }
+
+var weightInfo Info = Info{"weight", []string{"milligram", "gram", "kilogram", "ounce", "pound"}}
 
 var weightInGrams map[string]float64 = map[string]float64{
-	"gram":     1,
-	"kilogram": 1000,
-	"ounce":    28.35,
-	"pound":    453.592,
+	"milligram": .001,
+	"gram":      1,
+	"kilogram":  1000,
+	"ounce":     28.35,
+	"pound":     453.592,
 }
-
-var weightInfo Info = Info{"weight", []string{"gram", "kilogram", "ounce", "pound"}}
 
 var tempInCelsius map[string]func(float64, bool) float64 = map[string]func(float64, bool) float64{
 	"fahrenheit": func(num float64, from bool) float64 {
@@ -61,21 +65,45 @@ func main() {
 	errorTemplate = template.Must(template.ParseFiles("./files/templates/error.html", "./files/templates/header.html", "./files/templates/footer.html"))
 	resultTemplate = template.Must(template.ParseFiles("./files/templates/result.html", "./files/templates/header.html", "./files/templates/footer.html"))
 	formTemplate = template.Must(template.ParseFiles("./files/templates/form.html", "./files/templates/header.html", "./files/templates/footer.html"))
-
-	http.HandleFunc("GET /distance", handleDistForm)
+	http.HandleFunc("GET /{name}", handleAll)
+	http.HandleFunc("GET /", handleDistForm)
 	http.HandleFunc("POST /distance", handleDistResult)
-	http.HandleFunc("GET /weight", handleWeightForm)
 	http.HandleFunc("POST /weight", handleWeightResult)
-	http.HandleFunc("GET /temperature", handleTempForm)
 	http.HandleFunc("POST /temperature", handleTempResult)
-	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files/static"))))
+	http.Handle("GET /files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files/static"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleAll(w http.ResponseWriter, r *http.Request) {
+	path := r.PathValue("name")
+	switch path {
+	case "distance":
+		{
+			handleDistForm(w, r)
+		}
+	case "weight":
+		{
+			handleWeightForm(w, r)
+		}
+	case "temperature":
+		{
+			handleTempForm(w, r)
+		}
+	case "":
+		{
+			handleDistForm(w, r)
+		}
+	default:
+		{
+			errorPage(w, errors.New("invalid path"))
+		}
+	}
 }
 
 func handleDistForm(w http.ResponseWriter, r *http.Request) {
 	err := formTemplate.Execute(w, distInfo)
 	if err != nil {
-		log.Fatal(err)
+		errorPage(w, err)
 		return
 	}
 }
@@ -83,7 +111,7 @@ func handleDistForm(w http.ResponseWriter, r *http.Request) {
 func handleWeightForm(w http.ResponseWriter, r *http.Request) {
 	err := formTemplate.Execute(w, weightInfo)
 	if err != nil {
-		log.Fatal(err)
+		errorPage(w, err)
 		return
 	}
 }
@@ -91,7 +119,7 @@ func handleWeightForm(w http.ResponseWriter, r *http.Request) {
 func handleTempForm(w http.ResponseWriter, r *http.Request) {
 	err := formTemplate.Execute(w, tempInfo)
 	if err != nil {
-		log.Fatal(err)
+		errorPage(w, err)
 		return
 	}
 }
@@ -174,8 +202,8 @@ func handleTempResult(w http.ResponseWriter, r *http.Request) {
 }
 
 func errorPage(w http.ResponseWriter, err error) {
-	err = errorTemplate.Execute(w, err)
+	err = errorTemplate.Execute(w, err.Error())
 	if err != nil {
-		log.Fatal(err)
+		io.WriteString(w, "unable to load error page")
 	}
 }
